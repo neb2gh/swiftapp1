f804a18f338cef3872b143f51a71c49694e6221a
nate <neboccara@gmail.com>
2017-05-01 21:11:10 -0400

    1st commit

---
+++ SwiftApp1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftApp1
+//
+//  Created by Sam on 5/1/17.
+//  Copyright © 2017 nate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
